95ef6aaea64111ae7ebc9a247c19ace249dfa3cc
Narendra Singh <narendrasingh2036@gmail.com>
2024-06-12 22:21:04 +0530

    Add files via upload

---
+++ Pandas/Basic Select/1757. Recyclable and Low Fat Products.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+def find_products(products: pd.DataFrame) -> pd.DataFrame:
+    filtered_products=products[(products['low_fats'] == 'Y') & (products['recyclable'] == 'Y')]
+
+    result=filtered_products[['product_id']]
+
+    return result
+    
