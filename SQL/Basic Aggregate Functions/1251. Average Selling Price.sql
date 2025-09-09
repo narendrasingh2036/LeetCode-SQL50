7e5aea2542d0f6f7ff1c88628289f29795b58a00
Narendra Singh <narendrasingh2036@gmail.com>
2025-09-09 17:23:41 +0530

    Add files via upload

---
+++ SQL/Basic Aggregate Functions/1251. Average Selling Price.sql
@@ -0,0 +1,5 @@
+SELECT
+p.product_id,coalesce(round(sum(p.price*us.units) / sum(us.units),2),0) as average_price
+FROM Prices p
+LEFT JOIN UnitsSold us on p.product_id=us.product_id and us.purchase_date between p.start_date and p.end_date
+group by p.product_id
